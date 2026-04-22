af74922d549553dd39e2e4d8b6347444e52d47ed
Bhavesh <bhaveshjadhav718@gmail.com>
2026-04-22 18:50:07 +0530

    1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from packet_engine import analyze_pcap
 
 app = Flask(__name__)
 
-stats, packets = analyze_pcap("test_data.pcap")
+stats, packets = analyze_pcap("test_safe.pcap")
 
 @app.route("/")
 def home():
